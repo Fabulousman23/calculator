75ffdc298a55a6cd23cdb7a066897e82ff1e555d
Fabulousman23 <andrew.makiev@gmail.com>
2022-05-31 21:34:13 -0400

    change

---
+++ script.js
@@ -0,0 +1,20 @@
+let add = (num1, num2) => {
+   return sum = num1+num2;
+}
+add(1,2);
+
+let subtract = (num1, num2) => {
+   return sum = num1-num2;
+}
+add(1,2);
+
+let multiply = (num1, num2) => {
+   return sum = num1*num2;
+}
+add(1,2);
+
+let divide = (num1, num2) => {
+   return sum = num1/num2;
+}
+add(1,2);
+
